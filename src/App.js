4562479af59f7111b7c05146d09293a049c00fad
Aksh-007 <akshaybarapatre54@gmail.com>
2023-01-29 13:18:53 +0530

    routing react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './Style/App.css'
+import { BrowserRouter as Routes, Router, Route, BrowserRouter } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import About from './Components/About';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Style/Header.css"
+
+const Header = () => {
+  return (
+    <nav>
+         <a href="/">Home</a>
+         <a href="/about">About</a>
+         <a href="/contactus">Contact Us</a>
+         <a href="/login">Login</a>
+    </nav>
+   
+  )
+}
+
+export default Header
